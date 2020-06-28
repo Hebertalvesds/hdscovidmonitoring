09d58a84d565a038334010be6bcecedbe367763f
hebertalvesds <hebertalvesds@outlook.com>
2020-06-28 10:41:50 -0300

    Inclui o meta equiv para refresh automático. Nota. A solução é simples portanto para sua utilização esta adequação atende. Em outros casos seria melhor tratar com um reload dos componentes javascript.

--- scripts/app.js
+++ scripts/app.js
@@ -107,7 +107,6 @@ const app = new Vue({
         get_world_metrics.call(this);
         get_global_ranking.call(this);
         this.display_date_formated();
-        Metro.init();
         setInterval(function(){
             get_brazilian_metrics.call(this);
             get_world_metrics.call(this);
